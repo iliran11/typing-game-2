dd23a145e49f0ee2e87e81aec95f4b2564debebe
Liran Cohen - macbook <iliran11@gmail.com>
2018-10-23 18:25:33 +0300

    render game page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import './App.css';
-import GameManager from './components/game-manager/gameManagerContainer';
+import GamePage from './pages/GamePage';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route exact={true} path="/" component={GameManager} />
+        <Route exact={true} path="/" component={GamePage} />
       </Router>
     );
   }
---
+++ src/pages/GamePage.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import GameManagerContainer from '../components/game-manager/gameManagerContainer'
+
+export default function GamePage() {
+  return (
+    <GameManagerContainer/>
+  )
+}
