c80a550aa333206df176218c7583bb9ef6cc19c7
Liran Cohen - macbook <iliran11@gmail.com>
2018-10-23 22:42:32 +0300

    render game page - complete.

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,8 +1,12 @@
-import * as React from 'react'
-import GameManagerContainer from '../components/game-manager/gameManagerContainer'
+import * as React from 'react';
+import GameManagerContainer from '../components/game-manager/gameManagerContainer';
+import ScoreBoardContainer from '../components/Scoreboard/ScoreBoardContainer';
 
 export default function GamePage() {
   return (
-    <GameManagerContainer/>
-  )
+    <div>
+      <ScoreBoardContainer />
+      <GameManagerContainer />
+    </div>
+  );
 }
