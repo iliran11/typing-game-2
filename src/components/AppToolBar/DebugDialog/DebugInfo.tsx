adbce88fb639c3b6ebc21a94419ff3544764f704
Liran Cohen - macbook <iliran11@gmail.com>
2018-10-24 15:21:58 +0300

    update debug info and more infra stuff.

--- src/components/AppToolBar/DebugDialog/DebugInfo.tsx
+++ src/components/AppToolBar/DebugDialog/DebugInfo.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 
 export default function DebugInfo(props: any) {
+  const { myId, roomId, players, version } = props;
   return (
     <div style={{ padding: 50 }}>
       <h2>Debug Info</h2>
-      <p>
-        <b>My Id: </b>
-        {props.myId}
-      </p>
-      <p>
-        <b>Room Number: </b>
-        {props.roomId}
-      </p>
-      <p>
-        <b>Total Players in room: (not updates when a player leaving) </b>
-        {props.players.length}
-      </p>
+      <InfoLine title="My Id" value={myId || 'non yet'} />
+      <InfoLine title="Room Number" value={roomId} />
+      <InfoLine title="Total Players In Room" value={players.length} />
+      <InfoLine title="Max Players In Room" value={players.length} />
+      <InfoLine title="Client Version" value={version} />
     </div>
+  );
+}
+
+function InfoLine({ title, value }: { title: string; value: any }) {
+  return (
+    <p>
+      <b>{title}:</b>
+      <span>{` `}{value}</span>
+    </p>
   );
 }
--- src/components/AppToolBar/DebugDialog/DebugInfoContainer.ts
+++ src/components/AppToolBar/DebugDialog/DebugInfoContainer.ts
@@ -1,11 +1,14 @@
 import { connect } from 'react-redux';
 import DebugInfo from './DebugInfo';
+import packageJson from '../../../../package.json'
 
 const mapStateToProps = (state: any, ownProps: any) => {
   return {
     roomId: state.serverStatus.roomId,
     myId: state.serverStatus.myId,
-    players: state.serverStatus.players
+    players: state.serverStatus.players,
+    roomSize: state.serverStatus.roomSize,
+    version: packageJson.version
   };
 };
 
