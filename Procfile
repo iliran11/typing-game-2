5fc2d8dcb55c424434952b1ba40caf999df880d2
Liran Cohen - macbook <iliran11@gmail.com>
2018-10-26 10:13:12 +0300

    heroko updates

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./src/server-ts/dist/server-ts/src/server.js
--- src/server-ts/src/server.ts
+++ src/server-ts/src/server.ts
@@ -1,4 +1,13 @@
 import ServerManager from "./classes/ServerManager";
+import * as packageJs from '../package.json'
 
 
 const serverManager = ServerManager.getInstance();
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => res.send(`Server Version: ${packageJs.version}`))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
