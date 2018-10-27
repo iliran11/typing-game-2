79d9a995ff3eebf76655ef81d0d4c5ade4fe2a49
Liran Cohen - macbook <iliran11@gmail.com>
2018-10-27 12:18:53 +0300

    circular special colors logic

--- src/components/CircularProgress.tsx
+++ src/components/CircularProgress.tsx
@@ -3,15 +3,29 @@ import CircularProgressbar from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 export default function CircularProgress(props: any) {
+  // const percent = 25;
+  const percent = props.percentage;
   const styles = {
-    text: { fontSize: 30 }
+    text: { fontSize: 30 },
+    path: { stroke: getCircleColor(percent) }
   };
   return (
     <CircularProgressbar
       styles={styles}
       strokeWidth={15}
-      percentage={props.percentage * 100}
+      percentage={percent}
       text={`${props.text}`}
+      initialAnimation
     />
   );
+}
+
+function getCircleColor(percent: number) {
+  if (percent < 25) {
+    return '#B3DE81';
+  }
+  if (percent < 70) {
+    return '#68A225';
+  }
+  return '#265C00';
 }
