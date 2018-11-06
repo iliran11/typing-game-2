6fa03b2229beceb8c76c9f50c773012662412bcc
Liran Cohen - macbook <iliran11@gmail.com>
2018-11-06 10:12:50 +0200

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+docker container run \
+--env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+--env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  \
+-v $PWD/build:/data \
+garland/aws-cli-docker \
+aws s3 sync . s3://typing-game-dev --delete
