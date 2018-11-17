2002298837414927574ff1f6b4454a0f51a4f8af
Liran Cohen - macbook <iliran11@gmail.com>
2018-11-17 11:11:38 +0200

    onConnect styling

--- src/server-ts/src/event-handlers/onConnect.ts
+++ src/server-ts/src/event-handlers/onConnect.ts
@@ -1,17 +1,22 @@
-import * as io from "socket.io";
-import RoomManager from "../classes/RoomManager";
-import PlayerManager from "../classes/PlayerManager";
-import Player from "../classes/Player";
-import onDisconnect from "./onDisconnect";
-import {allocatePlayerToRoom,sendPlayerRoomInfo} from "./allocatePlayerToRoom";
-import playerTyping from "./playerTyping";
-import {ServerConnectSuccessPayload} from '../../../types'
-import onGameFinished from '../event-handlers/onGameFinished'
+import * as io from 'socket.io';
+import RoomManager from '../classes/RoomManager';
+import PlayerManager from '../classes/PlayerManager';
+import Player from '../classes/Player';
+import onDisconnect from './onDisconnect';
+import {
+  allocatePlayerToRoom,
+  sendPlayerRoomInfo
+} from './allocatePlayerToRoom';
+import playerTyping from './playerTyping';
+import { ServerConnectSuccessPayload } from '../../../types';
+import onGameFinished from '../event-handlers/onGameFinished';
+import onGameRestart from '../event-handlers/onGameRestart';
 import {
   PLAYER_TYPING,
   CONNECT_SERVER_SUCCESS,
-  GAME_HAS_FINISHED
-} from "../../../constants";
+  GAME_HAS_FINISHED,
+  RESTART_GAME
+} from '../../../constants';
 
 const roomManager = RoomManager.getInstance();
 const playerManager = PlayerManager.getInstance();
@@ -20,20 +25,23 @@ export default function onConnect(socket: io.Socket): void {
   // console.log(`connect- ${socket.client.id}`);
   const player = new Player(socket);
   playerManager.addPlayer(player);
-  const connectPayload : ServerConnectSuccessPayload = { myId: player.playerId }
-  socket.emit(CONNECT_SERVER_SUCCESS,connectPayload );
+  const connectPayload: ServerConnectSuccessPayload = { myId: player.playerId };
+  socket.emit(CONNECT_SERVER_SUCCESS, connectPayload);
   const room = allocatePlayerToRoom(socket);
-  sendPlayerRoomInfo(socket,room,player);
-    if (room.isGameActive) {
+  sendPlayerRoomInfo(socket, room, player);
+  if (room.isGameActive) {
     room.startGame();
   }
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     onDisconnect(socket);
   });
   socket.on(PLAYER_TYPING, data => {
     playerTyping(socket, data);
   });
-  socket.on(GAME_HAS_FINISHED,()=>{
+  socket.on(GAME_HAS_FINISHED, () => {
     onGameFinished(socket);
-  })
+  });
+  socket.on(RESTART_GAME, () => {
+    onGameRestart(socket);
+  });
 }
