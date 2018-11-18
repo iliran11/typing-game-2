0d7400e84e65c96f47f1b648eaf3d12d95ae0217
Liran Cohen - macbook <iliran11@gmail.com>
2018-11-18 22:55:24 +0200

    title styling

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,10 +8,21 @@ export default function Home(props: any): any {
     // console.log(props)
   };
   return (
-    <React.Fragment>
+    <div id="home-page">
+      <p>
+        Improve your typing skills,
+        <br />
+        by racing with your friends
+      </p>
       <Keyboard />
-      <button id="compete-now" className="button-large gradient-5" onClick={navigateToGame}>Compete Now</button>
-    </React.Fragment>
+      <button
+        id="compete-now"
+        className="button-large gradient-5"
+        onClick={navigateToGame}
+      >
+        Compete Now
+      </button>
+    </div>
   );
   // return <div id="home-page"><button onClick={navigateToGame}>Start!</button></div>;
 }
