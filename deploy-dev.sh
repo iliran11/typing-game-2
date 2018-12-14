0948c028028fdc259b9408530ba66e91c18828b5
Liran Cohen - macbook <iliran11@gmail.com>
2018-12-14 14:41:26 +0200

    auto public uploaded files

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,4 +3,4 @@ docker container run \
 --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  \
 -v $PWD/build:/data \
 garland/aws-cli-docker \
-aws s3 sync . s3://typing-game-dev --delete
+aws s3 sync . s3://typing-game-dev --delete --acl public-read
