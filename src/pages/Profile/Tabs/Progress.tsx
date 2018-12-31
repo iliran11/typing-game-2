6ab214efd9a6e24e6256c8d56f952b9717785a2d
Liran Cohen - macbook <iliran11@gmail.com>
2018-12-31 21:01:52 +0200

    progress tab

--- src/pages/Profile/Tabs/Progress.tsx
+++ src/pages/Profile/Tabs/Progress.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Chart from 'chart.js';
 
 export interface ProgressProps {}
 
@@ -8,13 +9,31 @@ export default class Progress extends React.Component<
   ProgressProps,
   ProgressState
 > {
+  myCanvas: any;
   constructor(props: ProgressProps) {
     super(props);
-
+    this.myCanvas = React.createRef();
     this.state = {};
   }
-
+  componentDidMount() {
+    var myChart = new Chart(this.myCanvas.current, {
+      type: 'line',
+      data: {
+        labels: ['jan','fab','march','april','may'],
+        datasets: [
+          {
+            label: 'hello world',
+            data: [1, 2, 3, 4, 50]
+          }
+        ]
+      }
+    });
+  }
   public render() {
-    return <div>progress</div>;
+    return (
+      <div style={{ padding: 50 }}>
+        <canvas ref={this.myCanvas} width="100" height="100" />
+      </div>
+    );
   }
 }
