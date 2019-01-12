5fd089b08be23479a98e52465e5a61c9a5151b30
Liran Cohen - macbook <iliran11@gmail.com>
2019-01-12 13:43:44 +0200

    if user has good token, but not appear in users collection, deny him

--- src/server-ts/src/ExpressControllers/verifyLoginController.ts
+++ src/server-ts/src/ExpressControllers/verifyLoginController.ts
@@ -1,5 +1,6 @@
 import { AUTH_HEADER_NAME } from '../../../constants';
 import { HandShakeData, LoginVerificationStatus } from '../../../types';
+import { User } from '../mongo/User/UserModel';
 const jwt = require('jsonwebtoken');
 import { User } from '../mongo/User/UserModel';
 
@@ -15,17 +16,28 @@ export default function verifyLoginController(req, res) {
       });
     }
     if (decoded) {
-      const handshakeData: HandShakeData = {
-        facebookId: decoded.id,
-        firstName: decoded.firstName,
-        lastName: decoded.lastName,
-        appToken
-      };
-      const response: LoginVerificationStatus = {
-        loginStatus: true,
-        handshakeData
-      };
-      res.status(200).send(response);
+      User.findById(decoded.id).then(userInDb => {
+        if (userInDb) {
+          const handshakeData: HandShakeData = {
+            facebookId: decoded.id,
+            firstName: decoded.firstName,
+            lastName: decoded.lastName,
+            appToken
+          };
+          const response: LoginVerificationStatus = {
+            loginStatus: true,
+            handshakeData
+          };
+
+          res.status(200).send(response);
+        } else {
+          // if it reaches here - maybe db got deleted?
+          res.status(400).send({
+            message:
+              'User Id does not exist (although token successfully decoded)'
+          });
+        }
+      });
     }
   });
 }
