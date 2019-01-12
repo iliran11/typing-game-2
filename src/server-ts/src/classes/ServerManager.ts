b8195f5f496489db85c8c402167b8739d1515781
Liran Cohen - macbook <iliran11@gmail.com>
2019-01-12 14:14:08 +0200

    socket check if user exist.

--- src/server-ts/src/classes/ServerManager.ts
+++ src/server-ts/src/classes/ServerManager.ts
@@ -1,6 +1,7 @@
 import * as io from 'socket.io';
 import onConnect from '../event-handlers/onConnect';
-import { Socket } from 'net';
+import { User } from '../mongo/User/UserModel';
+import { FacebookUserType } from '../../../types';
 const jwt = require('jsonwebtoken');
 
 export default class ServerManager {
@@ -12,12 +13,27 @@ export default class ServerManager {
     this.server.on('connection', onConnect);
     this.server.use((socket, next) => {
       const token = socket.handshake.query.token;
-      jwt.verify(token, process.env.SERVER_AUTH_SECRET, (err, decoded) => {
-        socket.handshake.userData = decoded
-        return next();
-        if (err) {
+      jwt.verify(
+        token,
+        process.env.SERVER_AUTH_SECRET,
+        (err, decoded: FacebookUserType) => {
+          User.findById(decoded.id).then(user => {
+            if (user) {
+              // updte the handshake as logged in user only if found in Users collection in mongo.
+              socket.handshake.userData = decoded;
+              return next();
+            } else {
+              return next();
+            }
+          }).catch((err)=>{
+            console.error(err);
+            return next():
+          })
+
+          if (err) {
+          }
         }
-      });
+      );
     });
   }
   get serverObject() {
