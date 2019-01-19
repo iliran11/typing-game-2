326d398cac19581daa90d609f17c54c76f4babc2
Liran Cohen - macbook <iliran11@gmail.com>
2019-01-19 02:27:18 +0200

    sentry

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,20 @@ import './index.css';
 import configureStore from './store/store';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://f56a1efc23bc47abb7b9807bf2fbf077@sentry.io/1374851',
+  environment: process.env.REACT_APP_ENV,
+  // @ts-ignore
+  integrations: [
+    new Sentry.Integrations.Breadcrumbs({
+      console: false
+    })
+  ]
+});
 
 const store = configureStore();
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
