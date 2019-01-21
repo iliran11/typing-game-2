108b9b7a2bcc5fc0e129c938830fc7697e5df13d
Liran Cohen - macbook <iliran11@gmail.com>
2019-01-22 01:16:30 +0200

    upgrade caching of index.html

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,4 +3,5 @@ docker container run \
 --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  \
 -v $PWD/build:/data \
 garland/aws-cli-docker \
-aws s3 sync . s3://typing-game-dev --delete --acl public-read
+aws s3 sync . s3://typing-game-dev --delete --acl public-read &&
+aws s3 cp index.html s3://typing-game-dev --metadata '{"Cache-Control":"no-cache"}'
