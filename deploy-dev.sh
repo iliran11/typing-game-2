e4a255d049cfaf8e6b62184a7fdb146c1d41d424
Liran Cohen - macbook <iliran11@gmail.com>
2019-01-22 02:16:55 +0200

    fixed script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,4 +4,4 @@ docker container run \
 -v $PWD/build:/data \
 garland/aws-cli-docker \
 aws s3 sync . s3://typing-game-dev --delete --acl public-read --exclude="index.html" &&
-aws s3 cp index.html s3://typing-game-dev --metadata '{"cache-control":"no-cache"}'
+aws s3 cp ./index.html s3://typing-game-dev --metadata '{"cache-control":"no-cache"}'
