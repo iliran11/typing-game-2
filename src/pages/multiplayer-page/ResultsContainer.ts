a48e5a358d364f8eebf1b9f572b61561f7a85a85
Liran Cohen - macbook <iliran11@gmail.com>
2019-02-21 09:04:15 +0200

    results container-now can access room id by going to replay if not exist

--- src/pages/multiplayer-page/ResultsContainer.ts
+++ src/pages/multiplayer-page/ResultsContainer.ts
@@ -1,16 +1,42 @@
 import { connect } from 'react-redux';
 import { MultiplayerResultPage } from './ResultsPage';
 import { ROOM_ID_PARM } from '../../constants';
-const mapDispatchToProps = {};
-const mapStateToProps = (state: any, props: any) => {
+import { PlayerGameStatus } from '../../types/typesIndex';
+import { RootState } from '../../types/typesIndex';
+import { fetchReplay } from '../../store/gameAction';
+
+const mapDispatchToProps = { fetchReplay };
+const mapStateToProps = (state: RootState, props: any) => {
   const queryString = require('query-string');
   const roomId = queryString.parse(props.location.search)[ROOM_ID_PARM];
+  const players = getPlayersStatus(state, roomId);
 
   return {
-    roomId
+    roomId,
+    myId: state.serverStatus.myId,
+    players
   };
 };
 export const MultiPlayerResultPage = connect(
   mapStateToProps,
   mapDispatchToProps
 )(MultiplayerResultPage);
+
+function getPlayersStatus(
+  state: RootState,
+  roomId: string
+): PlayerGameStatus[] {
+  const multiplayerRoom = state.multiplayerMapping[roomId];
+  if (multiplayerRoom) {
+    const result: PlayerGameStatus[] = [];
+    for (const playerStatus in multiplayerRoom.playersGameStatus) {
+      result.push(multiplayerRoom.playersGameStatus[playerStatus]);
+    }
+    return result;
+  }
+  const gameHistory = state.gamesHistory[roomId];
+  if (gameHistory) {
+    return gameHistory.players;
+  }
+  return [];
+}
--- src/pages/multiplayer-page/ResultsPage.tsx
+++ src/pages/multiplayer-page/ResultsPage.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react';
+import { PlayerGameStatus } from '../../types/typesIndex';
+import { networkManager } from '../../NetworkManager';
+import { BoxLoader } from '../../components/boxLoader/boxLoader';
 
 export interface MultiplayerResultPageProps {
   roomId: string;
+  myPosition: number;
+  players: PlayerGameStatus[];
+  fetchReplay: any;
+  myId: string;
 }
 
-export function MultiplayerResultPage(props: MultiplayerResultPageProps) {
-  return <div>{props.roomId}</div>;
+export class MultiplayerResultPage extends React.Component<
+  MultiplayerResultPageProps,
+  any
+> {
+  constructor(props: MultiplayerResultPageProps) {
+    super(props);
+    props.fetchReplay(props.roomId, props.myId);
+  }
+  public render() {
+    if (this.props.players.length === 0) {
+      return <BoxLoader message="Retrieving history ... " />;
+    }
+    return <div>hi</div>;
+  }
 }
