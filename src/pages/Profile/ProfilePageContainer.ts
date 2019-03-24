05d69b4d5e2db2c321764710b46857d4bd74bdf2
Liran Cohen - macbook <iliran11@gmail.com>
2019-03-25 00:25:20 +0200

    is data populated fix

--- src/pages/Profile/ProfilePageContainer.ts
+++ src/pages/Profile/ProfilePageContainer.ts
@@ -5,8 +5,9 @@ import { profileMainLoad } from '../../store/profileActions';
 
 const mapStateToProps = (state: RootState) => {
   const playerId = state.authentication.playerId;
+  const isPopulated = calculateDataPopulated(state, playerId);
   return {
-    isDataPopulated: true,
+    isDataPopulated: isPopulated,
     playerId,
     fullName: `${state.myData.firstName} ${state.myData.lastName}`
   };
@@ -19,3 +20,14 @@ export const MyProfilePageContainer = connect(
   mapStateToProps,
   mapDispatchToProps
 )(MyProfilePage);
+
+function calculateDataPopulated(state: RootState, playerId: string): boolean {
+  const dataBranch = state.bestGame[playerId];
+  if (!dataBranch) return false;
+  return Boolean(
+    dataBranch.multiplayer.desktop ||
+      dataBranch.multiplayer.mobile ||
+      dataBranch.typingTest.desktop ||
+      dataBranch.typingTest.mobile
+  );
+}
