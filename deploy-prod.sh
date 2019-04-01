8ca003f5b89f7c875d66796d88ae228068f3157b
Liran Cohen - macbook <iliran11@gmail.com>
2019-04-02 01:21:17 +0300

    deploy to prod

---
+++ deploy-prod.sh
@@ -0,0 +1,7 @@
+docker container run \
+--env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+--env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  \
+-v $PWD/build:/data \
+garland/aws-cli-docker \
+aws s3 sync . s3://typeit-prod --delete --acl public-read --exclude="index.html" &&
+aws s3 cp ./build/index.html s3://typeit-prod --cache-control max-age=0
